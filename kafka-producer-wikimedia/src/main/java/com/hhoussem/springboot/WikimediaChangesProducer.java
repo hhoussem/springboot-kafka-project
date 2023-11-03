17239e67dcf715183a3e9d9b4ef7a3ce65c5a3f9
hhoussem <houssemeddine.hamrouni@gmail.com>
2023-11-03 19:02:47 +0100

    Extract topic name to application.properties

--- kafka-producer-wikimedia/src/main/java/com/hhoussem/springboot/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/com/hhoussem/springboot/WikimediaChangesProducer.java
@@ -2,6 +2,7 @@ package com.hhoussem.springboot;
 
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,8 @@ public class WikimediaChangesProducer {
     public static final String URL_STREAM_RECENTCHANGE = "https://stream.wikimedia.org/v2/stream/recentchange";
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
 
     public WikimediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -21,9 +24,8 @@ public class WikimediaChangesProducer {
 
     public void sendMessage() throws InterruptedException {
         // to read real time stream data from wikimedia, we use event  source
-        String topic = "wikimedia_recentchange";
-        EventHandler enventHandler = new WikimediaChangesHandler(kafkaTemplate, topic);
-
+        EventHandler enventHandler = new WikimediaChangesHandler(kafkaTemplate, topicName);
+        System.out.println(topicName);
         EventSource eventSource = new EventSource.Builder(enventHandler, URI.create(URL_STREAM_RECENTCHANGE)).build();
         eventSource.start();
         TimeUnit.SECONDS.sleep(30);
